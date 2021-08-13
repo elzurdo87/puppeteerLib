200459f014c0874dce52cca9deabbfbc23aa36ae
Diego Ramirez <elzurdo87@gmail.com>
2021-08-13 10:41:32 -0300

    s

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const puppeteerLV2 = require('./puppeterLibV2');
-const scraper = require ('../puppeterLibV2');
+const scraper = require ('./puppeterLibV2');
 /*async function scraperRun(recipe, param, options) {
     let res = await puppeteerLV2.run(recipe,param,options);
     return res;
@@ -13,4 +13,3 @@ module.exports = {
     }
 } 
 
-scraperRun(recipe,param,options);
